9683afbf7405e3f607e5cba0be6f912abc5585bb
coolcao <me@coolcao.com>
2025-11-03 15:34:40 +0800

    完善关卡信息，以及关卡选择页面

--- src/app/chinese-puzzle/pages/level-select/level-select.component.ts
+++ src/app/chinese-puzzle/pages/level-select/level-select.component.ts
@@ -1,6 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { levels } from '../../data/data-set';
+import { Level } from '../../chinese-puzzle.type';
+
+interface GroupedLevels {
+  easy: Level[];
+  medium: Level[];
+  hard: Level[];
+}
 
 @Component({
   selector: 'app-level-select',
@@ -9,10 +16,25 @@ import { levels } from '../../data/data-set';
 })
 export class LevelSelectComponent implements OnInit {
   levels = levels;
+  groupedLevels: GroupedLevels = {
+    easy: [],
+    medium: [],
+    hard: []
+  };
 
   constructor(private router: Router) { }
 
-  ngOnInit() { }
+  ngOnInit() {
+    this.groupLevelsByDifficulty();
+  }
+
+  groupLevelsByDifficulty() {
+    this.groupedLevels = {
+      easy: this.levels.filter(level => level.difficulty === 'easy'),
+      medium: this.levels.filter(level => level.difficulty === 'medium'),
+      hard: this.levels.filter(level => level.difficulty === 'hard')
+    };
+  }
 
   selectLevel(levelId: string) {
     this.router.navigate(['fabric'], {
